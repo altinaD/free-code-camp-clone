c682807a5291ae97e6d14a1095da0ea68568ea58
altyn <altynberdiyeva99@gmail.com>
2023-09-07 16:38:45 +0500

    initial

---
+++ src/App.vue
@@ -0,0 +1,24 @@
+<template>
+  <Header />
+  <HelloWorld msg="Welcome to Your Vue.js App" />
+</template>
+
+<script>
+import HelloWorld from "./components/HelloWorld.vue";
+import Header from "./components/header/Header.vue";
+export default {
+  name: "App",
+  components: {
+    Header,
+    HelloWorld,
+  },
+};
+</script>
+
+<style>
+* {
+  box-sizing: border-box;
+  padding: 0;
+  margin: 0;
+}
+</style>
